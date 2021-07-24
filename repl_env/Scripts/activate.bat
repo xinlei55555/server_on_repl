dfcb55b28afc43fd72b367ca40ba1a53acf6424d
Xin Lei Lin <xinlei55555@gmail.com>
2021-07-24 18:50:14 -0400

    everything

--- repl_env/Scripts/activate.bat
+++ repl_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Random_Docs\CODES_EXPOSCIENCES\AAA\Back_end_good\VersionOfTheBackEndOnRepl\repl_env
+set VIRTUAL_ENV=D:\Random_Docs\CODES_EXPOSCIENCES\AAA\Back_end_good\1. VersionOfTheBackEndOnRepl\repl_env
 
 if not defined PROMPT set PROMPT=$P$G
 
